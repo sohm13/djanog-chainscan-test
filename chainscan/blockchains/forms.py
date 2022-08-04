f6f8cbe19c9b4183c4ec24c6e564465ea69b2cbb
sohm13 <32699621+sohm13@users.noreply.github.com>
2022-08-04 18:42:49 +0200

    in views.pair_balances отдает pandas df, pair_balance.html обрабатывает pandas df

--- chainscan/blockchains/forms.py
+++ chainscan/blockchains/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class PairFiletrForm(forms.Form):
 
-    pairs_address = forms.CharField()
-    block_chain = forms.CharField(max_length=10)
-    block_start = forms.IntegerField()
-    block_end = forms.IntegerField()
+    pairs_address = forms.CharField( initial = '0x16b9a82891338f9bA80E2D6970FddA79D1eb0daE')
+    block_chain = forms.CharField(max_length=10, initial = 'bsc')
+    block_start = forms.IntegerField(initial = 19395441)
+    block_end = forms.IntegerField(initial = 19395441+10)
--- chainscan/blockchains/views.py
+++ chainscan/blockchains/views.py
@@ -41,39 +41,46 @@ class BscDetailView(ListView):
 def pair_balances(response):
     if response.method == 'POST':
         form = PairFiletrForm(response.POST)
-        print(type(form), dir(form))
-        print(form.data)
-
 
         pairs_addresses = form.data['pairs_address'].split(',')
         block_start = int(form.data['block_start'])
         block_end = int(form.data['block_end'])
         pairs = [Pair(address=address, symbol=address) for address in pairs_addresses]
         
-        # pairs = [
-        #     Pair( address='0x16b9a82891338f9bA80E2D6970FddA79D1eb0daE', symbol='BUSD_WBNB'),
-        #     Pair( address='0x8FA59693458289914dB0097F5F366d771B7a7C3F', symbol='MBOX_WBNB'),
-
-        # ]
-        # 19395441
-        
         tik = time.time()
         bsc = get_bsc_chain_scan()
         transcations = bsc.get_scan_event_from_blocks(block_start, block_end, pairs)
-        print('time for resonse',time.time()-tik)
+        response_time = time.time()-tik
 
 
         prices = [int(t.reserve0) / int(t.reserve1) for t in transcations]
         pre_to_pandas = [obj.dict() for obj in transcations ]
         df = pd.DataFrame(pre_to_pandas)
-        df['price'] = df['reserve0']/ df['reserve1']
-        df = df[['block_number', 'price', 'pair_address']]
+        #columns: ['reserve0', 'reserve1', 'hash', 'block_number', 'transaction_index', 'pair_address', 'method'],
 
-  
+        pair_decimals = 18
+        df['reserve0'] = df['reserve0'] / (10 ** pair_decimals)
+        df['reserve1'] = df['reserve1'] / (10 ** pair_decimals)
+        df['price'] = df['reserve0']/ df['reserve1']
+        df['token0'] = df['reserve1'] - df['reserve1'].shift()
+        df['token1'] = df['reserve0'] - df['reserve0'].shift()
+        # columns for display
+        df = df[[
+            'block_number', 
+            'transaction_index', 
+            'price', 
+            'pair_address', 
+            'reserve0', 
+            'reserve1',
+            'token0',
+            'token1',
+            'hash'
+            ]]
+
         context = {
             'transcations': transcations,
-            'prices': prices,
-            'df': df.to_html(col_space=100)
+            'df': df,
+            'info':  {'response_time': response_time, 'len': len(df)}
         }
         return render(response, 'blockchains/pair_balance.html', context=context)
     
